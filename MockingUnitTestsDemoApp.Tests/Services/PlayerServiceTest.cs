29ee48fd12e963c55264207ec951328934a8a267
João Pedro Corrêa <joaopedrocorrea21@gmail.com>
2022-10-03 15:45:07 -0300

    🎉 feat: creating PlayerService tests

---
+++ MockingUnitTestsDemoApp.Tests/Services/PlayerServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockingUnitTestsDemoApp.Tests.Services
+{
+    public class PlayerServiceTest
+    {
+        [Fact]
+        public void GetForLeague_ExistingId_ReturnListOfPlayers()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void GetForLeague_ExistingIdButNoPlayersFound_ReturnEmptyListOfPlayers()
+        {
+
+        }
+
+        [Fact]
+        public void GetForLeague_NonExistingId_ReturnEmptyListOfPlayers()
+        {
+
+        }
+    }
+}
